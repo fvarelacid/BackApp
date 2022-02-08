19ee30450ae942a60c5dfb80ca866b18fe86b4af
Francisco <fvarelacid@gmail.com>
2022-02-08 10:11:17 +0000

    Updated to new Model

--- model.py
+++ model.py
@@ -6,18 +6,22 @@ from torch.nn import functional as F
 class DistressModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=5)
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=5, stride=(2, 2), padding=(2, 2))
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+        self.conv3 = nn.Conv2d(16, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+        self.conv4 = nn.Conv2d(32, 64, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
         self.conv2_drop = nn.Dropout2d()
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(2147392, 50)
+        self.fc1 = nn.Linear(10304, 50)
         self.fc2 = nn.Linear(50, 2)
 
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        # x = x.view(x.size(0), -1)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(F.max_pool2d(self.conv3(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv4(x)), 2))
+        #x = x.view(x.size(0), -1)
         x = self.flatten(x)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
