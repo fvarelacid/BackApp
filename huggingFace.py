aa6c2248ee2d53249aa4fbc3aa8d01ce50148945
Francisco <fvarelacid@gmail.com>
2022-02-08 01:13:01 +0000

    Update huggingFace.py

--- huggingFace.py
+++ huggingFace.py
@@ -1,22 +1,26 @@
 import librosa
 import torch
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
-import tensorflow
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 
-tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
+tokenizer = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
 model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
 
-#load any audio file of your choice
-speech, rate = librosa.load("data/voice_data/Test1.m4a",sr=16000)
+
+def get_transcription(audio_path):
+    #load any audio file of your choice
+    speech, rate = librosa.load("data/voice_data/Test1.m4a",sr=16000)
 
 
-input_values = tokenizer(speech, return_tensors = 'pt').input_values
+    input_values = tokenizer(speech, return_tensors = 'pt', sampling_rate=16000).input_values
 
-#Store logits (non-normalized predictions)
-logits = model(input_values).logits
+    #Store logits (non-normalized predictions)
+    logits = model(input_values).logits
 
-#Store predicted id's
-predicted_ids = torch.argmax(logits, dim =-1)
-#decode the audio to generate text
-transcriptions = tokenizer.decode(predicted_ids[0])
-print(transcriptions)
+
+    #Store predicted id's
+    predicted_ids = torch.argmax(logits, dim =-1)
+    #decode the audio to generate text
+    transcriptions = tokenizer.decode(predicted_ids[0])
+
+    return transcriptions
+
