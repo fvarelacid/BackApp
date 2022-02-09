cca7050b76cc70dbc94257b6932beeb92acf8fc7
Francisco <fvarelacid@gmail.com>
2022-02-09 23:40:23 +0000

    Update main.py
    Added normalization to features

--- main.py
+++ main.py
@@ -4,17 +4,34 @@ from model import DistressModel
 import torch
 import torchaudio
 from preprocessing import single_audio_preprocessing
+# from huggingFace import get_transcription
 
 ### Load Model
 model = DistressModel()
-model.load_state_dict(torch.load('output/best_model.pt', map_location='cpu'))
+model.load_state_dict(torch.load('output/best_model_2.pt', map_location='cpu'))
 model.eval
 
 ### Load Clip and Preprocess
-audio_pp = single_audio_preprocessing(file_path='data/FSD50K.dev_audio/443.wav', n_mels=64, n_fft=1024, hop_length=None)
-audio_pp = audio_pp.unsqueeze(1)
+audio_pp = single_audio_preprocessing(file_path='data/FSD50K.entire_data/61912.wav', n_mels=64, n_fft=1024, hop_length=None)
+X = audio_pp
+X_m, X_s = X.mean(), X.std()
+X = (X - X_m) / X_s
+X = X.unsqueeze(1)
+
 
 ### Predict
-pred = model(audio_pp)
+pred = model(X)
 print("Predicted:")
 print(f"{pred.argmax(1)}")
+
+
+# ------------------------------------------------------- #
+### Transcript Prediction ###
+
+# ### Words for help
+# words = [["LEAVE", "ME", "ALONE"], ["DON'T", "TOUCH", "ME"], ["GET", "AWAY", "FROM", "ME"], ["PLEASE", "DON'T", "DO", "IT"], 
+# ["GET", "OFF"], ["STOP", "IT"], ["STOP"], ["HELP", "ME"], ["HELP"], ["DON'T"],
+# ["WHAT", "YOU" ,"DOING"], ["GO", "AWAY"], ["LET", "ME", "GO"], ["PLEASE", "STOP"], ["PLEASE", "STOP", "IT"]]
+
+
+# print(get_transcription(audio_path))
